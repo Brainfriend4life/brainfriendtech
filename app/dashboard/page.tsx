bae6670d7bec0ef122faa7153ab10cb657f1a54b
brainfriend4life <emmanueloghenevovwero85@gmail.com>
2026-09-03 21:29:18 +0100

    add files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function DashboardPage() {
 
       {/* WhatsApp Group Banner */}
       <a
-        href="https://chat.whatsapp.com/HOEQ11RzvZ034U4FhX0GvU"
+        href="https://chat.whatsapp.com/Cc1bA5Czd1P8LxbebaMEO1"
         target="_blank"
         rel="noopener noreferrer"
         className="block w-full"
